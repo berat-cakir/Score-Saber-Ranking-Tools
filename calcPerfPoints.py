e1d84b53052353bc57fe26ccaff2e3ec9c83dfb0
Berat Cakir <berat.cakir@outlook.com>
2021-08-27 22:10:36 +0200

    Input implementation

--- calcPerfPoints.py
+++ calcPerfPoints.py
@@ -1,8 +1,12 @@
-averageScore = 250  # Your expected average performance points for each play
-weightDecay = 0.965  # Weight to consider for each consecutive ranked play
-numberOfPlays = 100  # Predict for n number of plays
+averageScore = float(input("Enter your expected average performance points per song: "))  # Your expected average performance points for each song
+weightDecay = float(input("Enter the weight to consider for each consecutive ranked song (default is 0.965): "))  # Weight to consider for each consecutive ranked song
+numberOfPlays = float(input("Enter the number of songs the scores to be calculated on: "))  # Predict for n number of songs
 
+print("\nSong no | Total performance points")
 totalScore = 0
-for i in range(1, numberOfPlays + 1):
+for i in range(1, int(numberOfPlays) + 1):
     totalScore += weightDecay**(i - 1) * averageScore
     print(f'{i}. {totalScore:3.2f}pp')
+   
+print(f'\nYour expected total performance points after playing {int(numberOfPlays)} songs equals {totalScore:3.2f}pp')
+input("\nPress ENTER to exit")
