2cb9acc2fb8719dbbbc4fbf4a0e88584d703b5b4
Berat Cakir <berat.cakir@outlook.com>
2021-08-29 08:16:08 +0200

    Building a repeat

--- calcPerfPoints.py
+++ calcPerfPoints.py
@@ -1,12 +1,22 @@
-averageScore = float(input("Enter your expected average performance points per song: "))  # Your expected average performance points for each song
-weightDecay = float(input("Enter the weight to consider for each consecutive ranked song (default is 0.965): "))  # Weight to consider for each consecutive ranked song
-numberOfPlays = float(input("Enter the number of songs the scores to be calculated on: "))  # Predict for n number of songs
+repeat = True
 
-print("\nSong no | Total performance points")
-totalScore = 0
-for i in range(1, int(numberOfPlays) + 1):
-    totalScore += weightDecay**(i - 1) * averageScore
-    print(f'{i}. {totalScore:3.2f}pp')
-   
-print(f'\nYour expected total performance points after playing {int(numberOfPlays)} songs equals {totalScore:3.2f}pp')
-input("\nPress ENTER to exit")
+while repeat:
+
+    averageScore = float(str(input("Enter your expected average performance points per song: ")).replace(",", "."))  # Your expected average performance points for each song
+    numberOfPlays = float(str(input("Enter the number of songs the scores to be calculated on: ")).replace(",", "."))  # Predict for n number of songs
+    if str(input("Do you want to use the standard weight (0.965) for each consecutive ranked song? (Y/N): ")).upper() == "N":
+        weightDecay = float(str(input("Enter the weight to consider for each consecutive ranked song (default is 0.965): ")).replace(",", "."))  # Weight to consider for each consecutive ranked song
+    else:
+        weightDecay = 0.965
+
+    print("\nSong no | Cumulated performance points")
+    totalScore = 0
+    for i in range(1, int(numberOfPlays) + 1):
+        totalScore += weightDecay**(i - 1) * averageScore
+        print(f"{i}. {totalScore:3.2f}pp")
+    
+    print(f"\nYour expected total performance points after playing {int(numberOfPlays)} songs equals {totalScore:3.2f}pp with a weight decay of {weightDecay}")
+    if str(input("\nPress ENTER to exit or R to start over: ")).upper() != "R":
+        repeat = False
+    else:
+        print()
